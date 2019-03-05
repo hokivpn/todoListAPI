0318000b89d21957f5518c62acd177a97d8e6bad
Daniel VM mc <hokivpn@hotmail.fr>
2019-03-05 12:02:48 +0100

    init

---
+++ app.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import db from './db/db';
+import bodyParser from 'body-parser';
+import router from './routes/index.js';
+
+// Set up the express app
+const app = express();
+const PORT = 5000;
+
+// Parse incoming requests data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(router);
+
+
+
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`)
+});
